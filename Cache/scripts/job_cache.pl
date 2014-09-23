0695c863df4673ec9d2154369a005532aeb8c377
Дмитрий Гуторкин <dgutorkin@gmail.com>
2014-09-23 14:31:04 +0400

    Cosmetics #3

--- Cache/scripts/job_cache.pl
+++ Cache/scripts/job_cache.pl
@@ -5,19 +5,26 @@ use Modern::Perl;
 use lib::abs("../lib");
 use Job::Cache;
  
-if ($ARGV[0]) {
-    my ($command, $key, $value) = @ARGV;
-    my $memd = Job::Cache->new( host => '127.0.0.1' );
-    if ($command eq 'set' and defined $key and defined $value) {
-        $memd->set($key, $value);
-    }
-    elsif ($command eq 'get' and defined $key) {
-        say $memd->get($key) || $memd->err;
-    }
-    elsif ($command eq 'delete' and defined $key) {
-        say $memd->delete($key) || $memd->err;
-    else {
-        die <<EOF;
+if (!$ARGV[0]) {
+    &usage;
+}
+
+my ($command, $key, $value) = @ARGV;
+&usage unless $command =~ /^(set|get|delete)$/;
+
+my $memd = Job::Cache->new( host => '127.0.0.1' );
+if ($command eq 'set' and defined $key and defined $value) {
+    $memd->set($key, $value);
+}
+elsif ($command eq 'get' and defined $key) {
+    say $memd->get($key) || $memd->err;
+}
+elsif ($command eq 'delete' and defined $key) {
+    say $memd->delete($key) || $memd->err;
+} 
+
+sub usage {
+    die <<EOF;
 Usage: $0 <command> [parameters]
 Command might be:
 \t set <key> <value> - to store data in memcached
